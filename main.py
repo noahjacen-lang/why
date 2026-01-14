29c6370219a5b8bef613fbfb454d64fb2b53486c
noahjacen-lang <noahjacen@gmail.com>
2026-01-14 00:00:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,24 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode="w")
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-
-
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!",intents=intents)
 
 @bot.event
 async def on_ready():
         print("Logged in as BitchBot")
-
 @bot.command()
 @commands.has_permissions(moderate_members=True) # Check if the command user has permission
 async def stfu(ctx: commands.Context, member: discord.Member):
     duration = datetime.timedelta(minutes=1)
-    """Server-mutes a user in a voice channel."""
+
     try:
         await member.timeout(duration)
         await ctx.send(f"{member.mention} is a bitch so they got muted.")
@@ -35,4 +33,4 @@ async def stfu(ctx: commands.Context, member: discord.Member):
         await ctx.send(f"Couldn't contain its oil: {e}")
 
 #Don't forget to run your bot with your token
-bot.run('MTQ2MDgyNTYzMTQ5ODY5ODkxNA.GtO98h.57v_lk0oOV4qxE1WMO0_RSBhUzpSPQWK3cP5xU', log_handler=handler, log_level=logging.INFO)
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
